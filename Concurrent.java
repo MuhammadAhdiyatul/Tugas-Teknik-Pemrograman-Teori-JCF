02aeca8f874112115b79c90460b9149b800aa301
MuhammadAhdiyatul <muhammad.ahdiyatul.tif425@polban.ac.id>
2026-03-06 20:11:02 +0700

    membuat code

--- Concurrent.java
+++ Concurrent.java
@@ -1,3 +1,12 @@
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Concurrent {
+    public static void main(String[] args){
+        ConcurrentHashMap<String, Integer> serverRequests = new ConcurrentHashMap<>();
+
+        serverRequests.put("Ipan", 1);
+        serverRequests.putIfAbsent("pahri", 1);
+
+        System.out.println("Data Server Requests:" + serverRequests);    }
     
 }
--- Record.java
+++ Record.java
@@ -1,3 +1,13 @@
+record ProfilMahasiswa(String nama, int umur, String nim, String jurusan){}
+
 public class Record{
-    
+    public static void main(String[] args){
+        ProfilMahasiswa mhs1 = new ProfilMahasiswa("Muhammad Zaki", 19, "123456789", "Teknik Pertambangan");
+        ProfilMahasiswa mhs2 = new ProfilMahasiswa("Ruzain Haikal", 18, "789654321", "Teknik Multimedia dan Jaringan");
+        ProfilMahasiswa mhs3 = new ProfilMahasiswa("Pahri Mufti", 19, "453216789", "Teknik Informatika");
+
+        System.out.println("Profil Mahasiswa 1:" + mhs1);
+        System.out.println("Profil Mahasiswa 2:" + mhs2);
+        System.out.println("Profil Mahasiswa 3:" + mhs3);
+    }
 }
