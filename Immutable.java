1fee1eaabf40b9b6687623fe715dc5ceadd3d467
MuhammadAhdiyatul <muhammad.ahdiyatul.tif425@polban.ac.id>
2026-03-06 20:27:12 +0700

    menambahkan code untuk immutable List, Set, Map

--- Immutable.java
+++ Immutable.java
@@ -1,3 +1,15 @@
+import java.util.List;
+import java.util.Set;
+import java.util.Map;
+
 public class Immutable {
+    public static void main(String[] args){
+        List<String> HariLibur = List.of("Sabtu", "Minggu");
+        Set<String> roleUser = Set.of("Dosen", "Mahasiswa", "Rektor");
+        Map<Integer, String> areaCode = Map.of(40001, "Bandung", 10001, "Jakarta");
     
+    System.out.println("Immutable List: " + HariLibur);
+    System.out.println("Immutable Set:" + roleUser);
+    System.out.println("Immutable Map:" + areaCode);
+}    
 }
