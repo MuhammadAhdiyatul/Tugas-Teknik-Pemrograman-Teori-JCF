aabebce43edfab706831219754160aa6506d98d3
MuhammadAhdiyatul <muhammad.ahdiyatul.tif425@polban.ac.id>
2026-03-06 21:03:49 +0700

    menambahkan code untuk queue dan deque

--- QueueDequeue.java
+++ QueueDequeue.java
@@ -1,3 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class QueueDequeue {
-    
+    public static void main(String[] args){
+        Queue<String> antreanBank = new LinkedList<>();
+        antreanBank.add("Nasabah 3");
+        antreanBank.add("Nasabah 2");
+        antreanBank.add("Nasabah 1");
+        System.out.println("Queue memproses :" + antreanBank.poll());
+
+        Deque<String> historyBrowser = new ArrayDeque<>();
+        historyBrowser.push("Google.com");
+        historyBrowser.push("GitHub.com");
+        System.out.println("Deque ( Tombol back ditekan):" + historyBrowser.pollFirst());
+    }
 }
