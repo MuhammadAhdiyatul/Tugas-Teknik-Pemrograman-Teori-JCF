cfd4abe6bdaeb097dbf813ae4856d6e36b8b4bc1
MuhammadAhdiyatul <muhammad.ahdiyatul.tif425@polban.ac.id>
2026-03-06 18:16:08 +0700

    membuat code untuk vector

--- Vektor.java
+++ Vektor.java
@@ -1,3 +1,15 @@
+import java.util.Vector;
+
 public class Vektor {
+    public static void main(String[] args){
+        Vector<String> Nama = new Vector<>();
+        Nama.add("Ipan");
+        Nama.add("Pahri");
+        Nama.add("Ruzain");
+
+        System.out.println("Daftar Nama: " + Nama);
+        System.out.println("Jumlah Nama: " + Nama.size());
+        System.out.println("Ambil Nama indeks ke 2: " + Nama.get(2));
+    }
     
 }
