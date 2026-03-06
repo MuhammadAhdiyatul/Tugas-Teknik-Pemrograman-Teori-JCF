6ea4a63f8484fcd0842245e21b5bff4ff48ab87a
MuhammadAhdiyatul <muhammad.ahdiyatul.tif425@polban.ac.id>
2026-03-06 20:46:30 +0700

    menambahkan code untruk optional

--- Opsional.java
+++ Opsional.java
@@ -1,3 +1,18 @@
+import java.util.Optional;
+
 public class Opsional {
-    
+    public static void main(String[] args){
+        Optional<String> hasilPencarian = cariData("User 1");
+        
+        hasilPencarian.ifPresentOrElse(
+            data -> System.out.println("Data ditemukan:" + data),
+            () -> System.out.println("Data tidak ditemukan."));
+    }    
+
+    static Optional<String> cariData(String id){
+        if (id.equals("User 1")){
+            return Optional.of("Data User 1");
+        }
+        return Optional.empty();
+    }
 }
